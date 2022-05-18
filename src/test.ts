432beedb08eba83c6d8b349e2f0022aaa60929a2
Scott Sadler <scott@livecontrol.io>
2022-05-18 17:59:30 -0400

    updated dependencies

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,5 @@
-// ESM module w/ default export broken
-import isPlainObject from "is-plain-object";
-
-// CJS module w/ default export broken
 import Decimal from "decimal.js";
+import { isPlainObject } from "is-plain-object";
 
-// CJS modules with separate @types broken
-import colors from "colors";
-
-console.log(isPlainObject({}));
 console.log(new Decimal(123.4567));
-console.log(colors.green("hello"));
+console.log(isPlainObject({}));
